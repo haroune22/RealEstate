5cea51d7850523be80856d3eddac0dc23f3ebee2
haroune22 <you@example.com>
2024-09-25 16:57:41 +0100

    changes

--- api/controllers/messege.controller.js
+++ api/controllers/messege.controller.js
@@ -1,7 +1,7 @@
 
 export const addMessage = (req, res) => {
     try {
-        
+        console.log("message added")
     } catch (error) {
         console.log(error);
         res.status(500).json({message: "server error"});
